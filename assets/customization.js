3dbd444a7b52cb519c118e60e6fae5fe40bddf0a
Jerome Borenstejn <jeromeborenstejn@gmail.com>
2023-03-09 19:27:25 +0100

    cleaning continue

--- assets/customization.js
+++ assets/customization.js
@@ -1,38 +1,7 @@
-// console.log('testboren')
-//
-var colorsAvailable = ["blanc", "orange", "bleu", "jaune", "rose", "gris", "vert", "violet", "noir", "vert-clair"];
-var crewneckColorsAvailable = ['pink', 'blue', 'grey', 'yellow'];
-var frenchCrewneckColorsAvailable = ['rose', 'bleu', 'gris', 'jaune'];
-var stainsColorsAvailable = ["green", "white", "black"];
-var wordsAvailable = $('#be-words input:radio').map(function () {
-    return this.value;
-})
+
 var defaultColor = "black";
 
-var colorStainsMapping = {
-    rose: "pink",
-    jaune: "yellow",
-    vertclair: "green",
-    bleu: "blue",
-    gris: 'grey'
-}
 
-var textColorHash = {
-    gris: "#86c9dd",
-    rose: "#f1ddaa",
-    bleu: "#43ad4e",
-    jaune: "#86c9dd",
-
-    noir: "pink",
-    blanc: "pink",
-    vert: "yellow",
-    // rose: "yellow",
-    // bleu: "orange",
-    // gris: "yellow",
-    // jaune: "grey",
-    orange: "yellow",
-    violet: defaultColor
-}
 
 var beColorHash = {
     gris: "white",
@@ -47,52 +16,9 @@ var beColorHash = {
     violet: "white"
 }
 
-let cloudinaryPath = "https://res.cloudinary.com/landly/image/upload/v1677583051/BE/";
-var srcPath = "https://cdn.shopify.com/s/files/1/0505/5751/7976/files/";
-var fullWords = "";
-var colorSelected = colorsAvailable[0];
-var stainsSelected = "none";
-var $customImage = $('.custom-image');
-var $customField = $('.custom-field');
-var $customText = $('.custom-text');
-var $beWord = $('.be-word');
-var $firstLine = $('#first-line');
-var $secondLine = $('#second-line');
-$customImage.hide();
-$(".custom-field" ).hide();
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded')
-    document.querySelector('#be-stains input[value="1"]').checked = true;
-
-});
 
 
-//
 
-function computeWord() {
-    var firstWord = $("#make-your-own").val().trim().replace(/\s+/g, '-').toLowerCase();
-    var secondWord = $("#make-your-own-2").val().trim().replace(/\s+/g, '-').toLowerCase();
-    fullWords = secondWord.length > 0 ? (firstWord + '-' + secondWord) : firstWord;
-    return fullWords
-}
-
-function fontColorFrom(color) {
-    return textColorHash[color];
-}
-
-function beColorFrom(color) {
-    return beColorHash[color];
-}
-
-function formatCustomWord(word) {
-    return word.replace('be', "<span class='be-word'>be</span>");
-}
-
-$('#be-stains input[type=radio]').change(function () {
-    console.log(this.value)
-    loadCurrentStains(this.value)
-});
 
 
 
--- assets/product-load.js
+++ assets/product-load.js
@@ -2,16 +2,40 @@ var $mainImages = $('.main-images');
 var $customImage = $('.custom-image');
 var $customField = $('.custom-field');
 var $customText = $('.custom-text');
-var $beWords = $('#be-words');
 var $pinkButton = $('.form-radio[value="Rose"]');
 var inCustomizationMode = false;
 
+let cloudinaryPath = "https://res.cloudinary.com/landly/image/upload/v1677583051/BE/";
+var srcPath = "https://cdn.shopify.com/s/files/1/0505/5751/7976/files/";
+var fullWords = "";
+var $beWord = $('.be-word');
+var $firstLine = $('#first-line');
+var $secondLine = $('#second-line');
+$customImage.hide();
+$customField.hide();
 
-function optionFrom(item) {
-    var itemUppercase = item.replaceAll('-', ' ').toUpperCase();
-    var string = "<input required class='required' type='radio' name='properties[choose your word]' value="+item+"> <span>"+itemUppercase+"</span><br>"
-    return string
-};
+var colorStainsMapping = {
+    rose: "pink",
+    jaune: "yellow",
+    vertclair: "green",
+    bleu: "blue",
+    gris: 'grey'
+}
+
+var textColorHash = {
+    gris: "#86c9dd",
+    rose: "#f1ddaa",
+    bleu: "#43ad4e",
+    jaune: "#86c9dd",
+    noir: "pink",
+    blanc: "pink",
+    vert: "yellow",
+    orange: "yellow",
+    violet: "black"
+}
+
+var crewneckColorsAvailable = ['pink', 'blue', 'grey', 'yellow'];
+var frenchCrewneckColorsAvailable = ['rose', 'bleu', 'gris', 'jaune'];
 
 function preloadImage(url) {
     const image = new Image();
@@ -45,16 +69,35 @@ function loadCurrentStains(optionSelected) {
     if (('[data-product-swatch="1"] .option--active .form-radio')[0] !== 'undefined') {
         colorSelected = $('[data-product-swatch="1"] .option--active .form-radio')[0].value.replace(/\s+/g, '').toLowerCase();
     }
-    console.log(colorSelected)
-    console.log("optionSelected")
-    console.log(optionSelected)
     setStainsSrc(colorSelected, optionSelected);
+}
+
+function computeWord() {
+    var firstWord = $("#make-your-own").val().trim().replace(/\s+/g, '-').toLowerCase();
+    var secondWord = $("#make-your-own-2").val().trim().replace(/\s+/g, '-').toLowerCase();
+    fullWords = secondWord.length > 0 ? (firstWord + '-' + secondWord) : firstWord;
+    return fullWords
+}
+
+function fontColorFrom(color) {
+    return textColorHash[color];
+}
+
+function beColorFrom(color) {
+    return beColorHash[color];
+}
+
+function formatCustomWord(word) {
+    return word.replace('be', "<span class='be-word'>be</span>");
 }
 
 
 if (myProductTitle.includes('customizable')) {
     console.log("myProductTitle");
 
+    $('#be-stains input[type=radio]').change(function () {
+        loadCurrentStains(this.value)
+    });
     $("#make-your-own").keyup(function () {
         $firstLine.empty();
         $firstLine.append(formatCustomWord(this.value.toLowerCase()));
@@ -79,7 +122,7 @@ if (myProductTitle.includes('customizable')) {
             $(this).val($(this).val().substr(0, max_chars));
         }
     });
-    
+
     $('input').keyup(function () {
         console.log('keyup')
         computeWord();
@@ -126,8 +169,7 @@ if (myProductTitle.includes('customizable')) {
             const url = `${baseStainsUrl}${color}-other.png`;
             preloadImage(url);
         });
-
-        console.log('images loaded')
         setStainsSrc(colorSelected, '1');
+        document.querySelector('#be-stains input[value="1"]').checked = true;
     })
 }
